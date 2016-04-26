1245100b841187474d9e21c4b4672671007dfd25
Luca Weiss <WEI16416@spengergasse.at>
2016-04-26 10:33:41 +0200

    Initial commit

---
+++ SportVereinsVerwaltung/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace SportVereinsVerwaltung
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string connectionString =
+          "Server=localhost;" +
+          "Database=schuldb;" +
+          "User ID=root;" +
+          "Pooling=false";
+       IDbConnection dbcon;
+       dbcon = new MySqlConnection(connectionString);
+       dbcon.Open();
+       IDbCommand dbcmd = dbcon.CreateCommand();
+
+       string sql =
+           "SELECT * " +
+           "FROM tunterricht";
+       dbcmd.CommandText = sql;
+       IDataReader reader = dbcmd.ExecuteReader();
+       while(reader.Read()) {
+            string FirstName = (string) reader["U_Raum"];
+            string LastName = (string) reader["U_Fach"];
+            Console.WriteLine("Name: " +
+                  FirstName + " " + LastName);
+       }
+       // clean up
+       reader.Close();
+       reader = null;
+       dbcmd.Dispose();
+       dbcmd = null;
+       dbcon.Close();
+       dbcon = null;
+    }
+  }
+}
+
+
